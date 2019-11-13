50549b2778525ca51b7e863c460c09ba6c788ec1
- - <mit@selbina.devel.thefuturetimes.com>
2019-11-13 13:33:03 +0900

    initial commit
    initial commit

---
+++ w32-imm32-on-start-enabler.el
@@ -0,0 +1,30 @@
+;;; w32-imm32-on-start-enabler.el --- Package that solves the problem that IMM32 does not work immediately after starting due to an Emacs bug
+
+;; Copyright (C) 2019 by TOGURO Mikito
+
+;; Author: TOGURO Mikito <mit@shalab.net>
+;; URL: https://github.com/maildrop/w32-imm32-on-start-enabler
+;; Versio: 0.01
+;; Package-Requires:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+
+;;; Code:
+
+
+(provide 'w32-imm32-on-start-enabler)
+;;; w32-imm32-on-start-enabler.el end here
